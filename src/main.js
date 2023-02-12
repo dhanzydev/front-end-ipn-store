3ea89bc3ba2fb081ca20f7ce810f07749a2af275
diptadhananjaya <madedipta88@gmail.com>
2023-02-12 16:01:00 +0800

    icon fontawesome

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "@/plugins/icon";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
