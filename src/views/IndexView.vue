1a22fb706b19b7a0dfd63238e067ecd477316d6f
diptadhananjaya <madedipta88@gmail.com>
2023-02-16 00:37:38 +0800

    add route and fix logo in footer

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -86,46 +86,50 @@
         </div>
       </div>
       <div class="d-flex justify-content-end">
-        <router-link to="/" class="text-decoration-none btn btn-primary mt-5"
+        <router-link
+          to="/produk"
+          class="text-decoration-none btn btn-primary mt-5"
           >Lihat Selengkapnya</router-link
         >
       </div>
     </div>
   </section>
-  <div class="bg-location mt-5">
-    <div class="container py-5">
-      <div class="row">
-        <div class="col-lg-6 col-12 order-2 order-lg-0">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d494.5424201288801!2d112.58127254596533!3d-7.427646000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78098526ac4dab%3A0x2a5f411352cd3c0f!2sCV.Bambang%20Jaya!5e0!3m2!1sen!2sid!4v1676303578369!5m2!1sen!2sid"
-            style="border: 0"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-            class="w-100"
-            height="620"
-          ></iframe>
-        </div>
-        <div class="col-lg-6 col-12 order-1 order-lg-0">
-          <h2 class="fw-bold">Kunjungi Toko Offline Kami</h2>
-          <p class="fw-medium address mt-5">
-            Griya Krian Residence blok A Lotus No. 25,Krian, Sidoarjo,Jawa
-            Timur,Indonesia, Perumahan Puri Bagus, Sadenganmijen, Kec. Krian,
-            Kabupaten Sidoarjo, Jawa Timur 61262
-          </p>
-          <p class="fw-bold fs-4">Jam Buka</p>
-          <div class="schedule">
-            <p>Senin : 08:00 - 17:00 WIB</p>
-            <p>Selasa : 08:00 - 17:00 WIB</p>
-            <p>Rabu : 08:00 - 17:00 WIB</p>
-            <p>Kamis : 08:00 - 17:00 WIB</p>
-            <p>Jumat : 08:00 - 17:00 WIB</p>
-            <p>Sabtu : 08:00 - 17:00 WIB</p>
+  <section>
+    <div class="bg-location mt-5">
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-lg-6 col-12 order-2 order-lg-0">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d494.5424201288801!2d112.58127254596533!3d-7.427646000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78098526ac4dab%3A0x2a5f411352cd3c0f!2sCV.Bambang%20Jaya!5e0!3m2!1sen!2sid!4v1676303578369!5m2!1sen!2sid"
+              style="border: 0"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+              class="w-100"
+              height="620"
+            ></iframe>
+          </div>
+          <div class="col-lg-6 col-12 order-1 order-lg-0">
+            <h2 class="fw-bold">Kunjungi Toko Offline Kami</h2>
+            <p class="fw-medium fs-5 mt-5">
+              Griya Krian Residence blok A Lotus No. 25,Krian, Sidoarjo,Jawa
+              Timur,Indonesia, Perumahan Puri Bagus, Sadenganmijen, Kec. Krian,
+              Kabupaten Sidoarjo, Jawa Timur 61262
+            </p>
+            <p class="fw-bold fs-4 f">Jam Buka</p>
+            <div class="schedule">
+              <p>Senin : 08:00 - 17:00 WIB</p>
+              <p>Selasa : 08:00 - 17:00 WIB</p>
+              <p>Rabu : 08:00 - 17:00 WIB</p>
+              <p>Kamis : 08:00 - 17:00 WIB</p>
+              <p>Jumat : 08:00 - 17:00 WIB</p>
+              <p>Sabtu : 08:00 - 17:00 WIB</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 <script setup>
 import CardCategory from "../components/CardCategory.vue";
@@ -154,11 +158,6 @@ import CardProduct from "../components/CardProduct.vue";
 .bg-location {
   background-color: rgba(69, 117, 210, 0.31);
 }
-
-.address {
-  font-size: 23px;
-}
-
 .schedule {
   font-weight: normal;
   font-size: 20px;
